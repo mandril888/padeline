d73322c33907fc8290ed155d5749d6b84221ccc8
mandril888 <arturo.rubio.89.8@gmail.com>
2024-04-13 16:12:59 +0200

    autentication base

---
+++ src/main/java/com/ironhack/padeline/controllers/interfaces/RoleControllerInterface.java
@@ -0,0 +1,20 @@
+package com.ironhack.padeline.controllers.interfaces;
+
+import com.ironhack.padeline.dtos.RoleToUserDTO;
+import com.ironhack.padeline.models.Role;
+
+public interface RoleControllerInterface {
+    /**
+     * Save a new role
+     *
+     * @param role the role to be saved
+     */
+    void saveRole(Role role);
+
+    /**
+     * Add a role to a user
+     *
+     * @param roleToUserDTO object containing the username and role name to be added to the user
+     */
+    void addRoleToUser(RoleToUserDTO roleToUserDTO);
+}
